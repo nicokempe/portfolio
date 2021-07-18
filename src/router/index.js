c15b5399e03da843a621f8c2c8efc79eef0dd24d
Nico Kempe <50241630+nicokempe@users.noreply.github.com>
2021-07-18 23:22:05 +0200

    Added the views (web pages)

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Home from '../views/HomeView.vue'
 
 Vue.use(VueRouter)
 
@@ -11,13 +11,43 @@ const routes = [
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
+    path: '/contact',
+    name: 'Contact',
     // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
+    // this generates a separate chunk (contact.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: function () {
-      return import(/* webpackChunkName: "about" */ '../views/About.vue')
+      return import(/* webpackChunkName: "contact" */ '../views/ContactView.vue')
+    }
+  },
+  {
+    path: '/policies',
+    name: 'Policies',
+    // route level code-splitting
+    // this generates a separate chunk (policies.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: function () {
+      return import(/* webpackChunkName: "policies" */ '../views/PoliciesView.vue')
+    }
+  },
+  {
+    path: '/imprint',
+    name: 'Imprint',
+    // route level code-splitting
+    // this generates a separate chunk (imprint.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: function () {
+      return import(/* webpackChunkName: "imprint" */ '../views/ImprintView.vue')
+    }
+  },
+  {
+    path: '/projects',
+    name: 'Projects',
+    // route level code-splitting
+    // this generates a separate chunk (projects.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: function () {
+      return import(/* webpackChunkName: "projects" */ '../views/ProjectsView.vue')
     }
   }
 ]
