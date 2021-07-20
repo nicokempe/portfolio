f81693aa1a074013c9048bb461186cc459fde39b
Nico Kempe <50241630+nicokempe@users.noreply.github.com>
2021-07-20 19:36:51 +0200

    Installed the Vue-i18n Plugin
    + Added the .env file
    + Package changes
      - package-lock.json
      - package.json
    + i18n.js file (auto generated)
    + Added i18n to the main.js Vue method
    + Added a demo undefined.json file
    + Added the vue.config.js

---
+++ vue.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  pluginOptions: {
+    i18n: {
+      locale: undefined,
+      fallbackLocale: undefined,
+      localeDir: undefined,
+      enableInSFC: undefined
+    }
+  }
+}
