a788553befec20cddfe14c3aa33413a64dbfb64a
Nico Kempe <50241630+nicokempe@users.noreply.github.com>
2021-07-21 21:55:01 +0200

    Added meta data to the Vue config
    - projectName
    - url
    - description
    - twitterHandle
    - socialImage
    - googleAnalytics

--- vue.config.js
+++ vue.config.js
@@ -5,6 +5,14 @@ module.exports = {
       fallbackLocale: undefined,
       localeDir: undefined,
       enableInSFC: undefined
+    },
+    meta: {
+      projectName: 'Portfolio',
+      url: 'https://www.nicokempe.de/',
+      description: 'My Name is Nico Kempe and I create software in my spare time.',
+      twitterHandle: 'NicoVRNY',
+      socialImage: 'src/assets/logo.png',
+      googleAnalytics: 'G-123456789',
     }
   }
 }
