6da0746dbb114be83f4e3130dafbe324eee0fd1c
Nico Kempe <50241630+nicokempe@users.noreply.github.com>
2021-09-13 19:34:23 +0200

    Add a temp footer

--- src/components/global/FooterComponent.vue
+++ src/components/global/FooterComponent.vue
@@ -1,4 +1,22 @@
 <template>
-  <v-container>
-  </v-container>
+  <v-footer bottom fixed padless class="primary">
+    <v-container>
+      <v-card
+          flat
+          rounded
+          class="primary lighten-1 white--text text-center"
+      >
+
+        <v-card-text class="white--text pt-5">
+          Phasellus feugiat arcu sapien, et iaculis ipsum elementum sit amet. Mauris cursus commodo interdum. Praesent ut risus eget metus luctus accumsan id ultrices nunc. Sed at orci sed massa consectetur dignissim a sit amet dui. Duis commodo vitae velit et faucibus. Morbi vehicula lacinia malesuada. Nulla placerat augue vel ipsum ultrices, cursus iaculis dui sollicitudin. Vestibulum eu ipsum vel diam elementum tempor vel ut orci. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+        </v-card-text>
+
+        <v-divider></v-divider>
+
+        <v-card-text class="white--text">
+          {{ new Date().getFullYear() }} — <strong>Nico Kempe Einzelunternehmen</strong>
+        </v-card-text>
+      </v-card>
+    </v-container>
+  </v-footer>
 </template>
