cc93045c8b861495e0371f7081a231a2b040f2f4
Nico Kempe <50241630+nicokempe@users.noreply.github.com>
2021-09-13 19:35:11 +0200

    Add text to the 404 page

--- src/views/404View.vue
+++ src/views/404View.vue
@@ -1,5 +1,22 @@
 <template>
-  <div class="404">
-    <h1>This is an 404 page</h1>
-  </div>
+  <v-container>
+    <v-row class="text-center">
+
+      <v-col class="mb-4">
+        <h1 class="display-2 font-weight-bold mb-3">
+          404
+        </h1>
+
+        <p class="subheading font-weight-regular">
+          This page could not be found, check out
+          <br>my landing page and search again.
+          <br>
+          <br>
+          <v-btn class="primary">Home
+            <router-link to="/" tag="button"></router-link>
+          </v-btn>
+        </p>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
